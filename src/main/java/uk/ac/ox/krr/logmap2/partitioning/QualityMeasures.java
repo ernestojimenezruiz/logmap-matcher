64ea2b35e79999fa9cc55b43ac76b98f37304be8
ernestoj <ernestoj@ifi.uio.no>
2018-02-27 22:43:25 +0100

    Some additions to quality measures for partitioning

--- src/main/java/uk/ac/ox/krr/logmap2/partitioning/QualityMeasures.java
+++ src/main/java/uk/ac/ox/krr/logmap2/partitioning/QualityMeasures.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import static java.lang.Math.*;
+
 import uk.ac.ox.krr.logmap2.mappings.objects.MappingObjectStr;
 
 /**
@@ -81,8 +83,93 @@ public class QualityMeasures {
 	public QualityMeasures(List<MatchingTask> tasks, Set<MappingObjectStr> alignment){
 		this.tasks=tasks;
 		this.alignment=alignment;
+		
 	}
 	
+	
+	
+	public void computeMetricsModuleTasks(List<MatchingTask> tasks){
+	
+		int aux;
+		double ratio;
+		
+		number_tasks = tasks.size();
+		aggregation_modules_ontology1=0;
+		aggregation_modules_ontology2=0;		
+		
+	
+		min_size_modules = min(tasks.get(0).getSignatureSourceOntology().size(), tasks.get(0).getSignatureTargetOntology().size());
+		max_size_modules = max(tasks.get(0).getSignatureSourceOntology().size(), tasks.get(0).getSignatureTargetOntology().size());
+		average_size_modules=0;
+		
+		min_ratio_modules = (double)tasks.get(0).getSignatureSourceOntology().size()/(double)tasks.get(0).getSignatureTargetOntology().size();
+		max_ratio_modules = (double)tasks.get(0).getSignatureSourceOntology().size()/(double)tasks.get(0).getSignatureTargetOntology().size();
+		average_ratio_modules = 0;
+		
+		for (int i=0; i<tasks.size(); i++){			
+			
+			aggregation_modules_ontology1+=tasks.get(i).getSignatureSourceOntology().size();
+			aggregation_modules_ontology2+=tasks.get(i).getSignatureTargetOntology().size();
+			
+			
+			aux = min(tasks.get(i).getSignatureSourceOntology().size(), tasks.get(i).getSignatureTargetOntology().size());
+			if (aux<min_size_modules)
+				min_size_modules = aux;
+			
+			aux = max(tasks.get(i).getSignatureSourceOntology().size(), tasks.get(i).getSignatureTargetOntology().size());
+			if (aux>max_size_modules)
+				max_size_modules = aux;
+			
+			average_size_modules += tasks.get(i).getSignatureSourceOntology().size() + tasks.get(i).getSignatureTargetOntology().size();
+			
+			
+			ratio = (double)tasks.get(i).getSignatureSourceOntology().size()/(double)tasks.get(i).getSignatureTargetOntology().size();
+			if (ratio<min_ratio_modules)
+				min_ratio_modules=ratio;
+			else if (ratio>max_ratio_modules)
+				max_ratio_modules=ratio;
+			
+			average_ratio_modules += ratio;
+			
+		}
+		
+		average_size_modules = (double)average_size_modules/(double)tasks.size()*2;
+		
+		average_ratio_modules = (double)average_ratio_modules/(double)tasks.size();
+		
+	}
+	
+	
+	
+	
+	public void computeSizeTasks(List<MatchingTask> tasks){
+		
+		avg_size_task=0;
+		
+		aggregation_task_sizes=0;
+		
+		int size_task  = tasks.get(0).getSignatureSourceOntology().size() * tasks.get(0).getSignatureTargetOntology().size(); //m X n;
+		
+		min_size_task = size_task;
+		max_size_task = size_task;
+		
+		for (int i=0; i<tasks.size(); i++){	
+		
+			size_task = tasks.get(i).getSignatureSourceOntology().size() * tasks.get(i).getSignatureTargetOntology().size(); //m X n;
+			
+			if (size_task<min_size_task)
+				min_size_task=size_task;
+			else if (size_task>max_size_task)
+				max_size_task=size_task;
+			
+			aggregation_task_sizes+=size_task;
+			
+		}
+		
+		avg_size_task = (double)aggregation_task_sizes/(double)tasks.size();
+		
+		
+	}
 	
 	
 	
@@ -91,7 +178,8 @@ public class QualityMeasures {
 		int positive_hits=0;
 		
 		for (MappingObjectStr mapping : alignment){
-			if (task.getSignatureSourceOntology().contains(mapping.getIRIStrEnt1()) && task.getSignatureTargetOntology().contains(mapping.getIRIStrEnt2())){
+			//if (task.getSignatureSourceOntology().contains(mapping.getIRIStrEnt1()) && task.getSignatureTargetOntology().contains(mapping.getIRIStrEnt2())){
+			if (isMappingAvailableInTask(task, mapping)){
 				positive_hits++;
 			}			
 		}
@@ -101,7 +189,13 @@ public class QualityMeasures {
 	}
 	
 	
-	public double getGlobalCoverage(List<MatchingTask> tasks, Set<MappingObjectStr> alignment){
+	/**
+	 * Computes global coverage and also local values
+	 * @param tasks
+	 * @param alignment
+	 * @return global coverage
+	 */
+	public double computeCoverageTasks(List<MatchingTask> tasks, Set<MappingObjectStr> alignment){
 		
 		int positive_hits=0;
 		
@@ -127,7 +221,7 @@ public class QualityMeasures {
 				
 				if (isMappingAvailableInTask(task, mapping)){
 					positive_hits_task.set(i, positive_hits_task.get(i) + 1); //local coverage
-					positive_hits++;
+					positive_hits++; //global coverage
 				}
 				else if (isPartialMappingAvailableInTask(task, mapping)){
 					negative_redundancy++;
@@ -143,16 +237,37 @@ public class QualityMeasures {
 		
 		
 		//Local coverage
+		min_coverage_task=positive_hits_task.get(0);
+		max_coverage_task=positive_hits_task.get(0);
+		average_coverage_task=0;
+		for (int i=0; i<positive_hits_task.size(); i++){
+			
+			if (positive_hits_task.get(i)>max_coverage_task)
+				max_coverage_task=positive_hits_task.get(i);
+			else if (positive_hits_task.get(i) < min_coverage_task)
+				min_coverage_task=positive_hits_task.get(i);
+			
+			average_coverage_task+=positive_hits_task.get(i);
+			
+		}
 		
+		
+		average_coverage_task = (double)average_coverage_task/(double)tasks.size();
+		average_coverage_task = (double)average_coverage_task/(double)alignment.size();
+		
+		max_coverage_task = (double)max_coverage_task/(double)alignment.size();
+		min_coverage_task = (double)min_coverage_task/(double)alignment.size();	
 		
 		
 		
 		
 		//Global coverage
+		positive_hits=0;
 		for (int hits : positive_hits_list){
 			
 			if (hits>0)
 				positive_hits++;
+			
 			if (hits>1)
 				positive_redundancy++;
 			
